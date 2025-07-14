2ba60f028bfe4785cd84be860706120a3db79a0e
Kevin Vu <kevin.m.vu@gmail.com>
2025-07-13 21:45:11 -0700

    Refactor collision detection into HasCollided

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -85,25 +85,26 @@ namespace DrivingGameV2 {
             }
         }
 
+        private static bool HasCollided(Collider a, Collider b) {
+            // I use Physics.ComputePenetration because I was having issues with the more commonly used Collider.OnTriggerEnter.
+            // When the car collided with a barrier right next to its reset position and the reset timeout was too low, OnTriggerEnter
+            // would fail to trigger because the collisions happened too frequently (within 10 ms or less) and collision checking was tied
+            // to the 50 Hz FixedUpdate. This meant OnTriggerEnter wouldn't always trigger. Increasing the FixedUpdate frequency resolved this,
+            // but I think it's more straightforward to do inline collision checking with ComputePenetration.
+            return Physics.ComputePenetration(
+                a, a.transform.position, a.transform.rotation,
+                b, b.transform.position, b.transform.rotation,
+                out _, out _);
+        }
+
         private bool HasCarCollidedWithBarrier() {
             List<Collider> barrierColliders = GameObject.FindGameObjectsWithTag(Tags.Barrier.ToString()).Select(x => x.GetComponent<Collider>()).ToList();
             Assert.IsFalse(barrierColliders.Any(x => x is null));
             foreach (Collider barrierCollider in barrierColliders) {
-                // Here I use Physics.ComputePenetration because I was having issues with the more commonly used Collider.OnTriggerEnter.
-                // When the car collided with a barrier right next to its reset position and the reset timeout was too low, OnTriggerEnter
-                // would fail to trigger because the collisions happened too frequently (within 10 ms or less) and collision checking was tied
-                // to the 50 Hz FixedUpdate. This meant OnTriggerEnter wouldn't always trigger. Increasing the FixedUpdate frequency resolved this,
-                // but I think it's more straightforward to do inline collision checking with ComputePenetration.
-                bool isOverlapping =
-                    Physics.ComputePenetration(
-                        this.CarCollider, this.CarCollider.transform.position, this.CarCollider.transform.rotation,
-                        barrierCollider, barrierCollider.transform.position, barrierCollider.transform.rotation,
-                        out _, out _);
-                if (isOverlapping) {
+                if (HasCollided(this.CarCollider, barrierCollider)) {
                     return true;
                 }
             }
-
             return false;
         }
 
