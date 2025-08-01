2929495998ce8886db9fbcb932d9740bf3b8897a
Kevin Vu <kevin.m.vu@gmail.com>
2025-07-31 17:11:29 -0700

    Disable brake input. Tune standard car dynamics.

--- Unity/Assets/Scripts/DataObjects/Input.cs
+++ Unity/Assets/Scripts/DataObjects/Input.cs
@@ -15,6 +15,6 @@ namespace DrivingGameV2 {
         public static bool PrevCarEvent => PlayerActions.CarSwitchPrev.WasPressedThisFrame();
         public static bool ResetCarEvent => PlayerActions.CarReset.WasPressedThisFrame();
         public static Vector2 Accel => PlayerActions.CarAccel.ReadValue<Vector2>();
-        public static float Brake => PlayerActions.CarBrake.ReadValue<float>();
+        public static float Brake => 0;
     }
 }
