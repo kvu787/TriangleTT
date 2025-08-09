a97603c9889bccd92356b393940eec847f3298a9
Kevin Vu <kevin.m.vu@gmail.com>
2025-08-08 19:22:19 -0700

    Minor refactor

--- TriangleTT/Assets/Scripts/Scene/SceneObjects.cs
+++ TriangleTT/Assets/Scripts/Scene/SceneObjects.cs
@@ -12,13 +12,15 @@ namespace TriangleTT {
         public Collider _CheckpointCollider2;
         public static Collider CheckpointCollider3;
         public Collider _CheckpointCollider3;
-
         public static GameObject PlaceholderCarObject;
         public GameObject _PlaceholderCarObject;
-
         public static Transform CameraAnglePivot;
         public Transform _CameraAnglePivot;
+        public static Light Sunlight;
+        public Light _Sunlight;
 
+        public static TMP_Text FpsTextLabel;
+        public TMP_Text _FpsTextLabel;
         public static Button OpenMenuButton;
         public Button _OpenMenuButton;
         public static GameObject Menu;
@@ -32,32 +34,22 @@ namespace TriangleTT {
         public static Toggle ShadowToggle;
         public Toggle _ShadowToggle;
 
-        public static Light Sunlight;
-        public Light _Sunlight;
-
-        public static TMP_Text FpsTextLabel;
-        public TMP_Text _FpsTextLabel;
-
         public void Init() {
             UnityNullExtensions.AssignIfNull(ref FinishLineCollider, this._FinishLineCollider);
             UnityNullExtensions.AssignIfNull(ref CheckpointCollider1, this._CheckpointCollider1);
             UnityNullExtensions.AssignIfNull(ref CheckpointCollider2, this._CheckpointCollider2);
             UnityNullExtensions.AssignIfNull(ref CheckpointCollider3, this._CheckpointCollider3);
-
             UnityNullExtensions.AssignIfNull(ref PlaceholderCarObject, this._PlaceholderCarObject);
-
             UnityNullExtensions.AssignIfNull(ref CameraAnglePivot, this._CameraAnglePivot);
+            UnityNullExtensions.AssignIfNull(ref Sunlight, this._Sunlight);
 
+            UnityNullExtensions.AssignIfNull(ref FpsTextLabel, this._FpsTextLabel);
             UnityNullExtensions.AssignIfNull(ref OpenMenuButton, this._OpenMenuButton);
             UnityNullExtensions.AssignIfNull(ref Menu, this._Menu);
             UnityNullExtensions.AssignIfNull(ref CloseMenuButton, this._CloseMenuButton);
             UnityNullExtensions.AssignIfNull(ref LapTimesFilePathInputField, this._LapTimesFilePathInputField);
             UnityNullExtensions.AssignIfNull(ref DebugLogFilePathInputField, this._DebugLogFilePathInputField);
             UnityNullExtensions.AssignIfNull(ref ShadowToggle, this._ShadowToggle);
-
-            UnityNullExtensions.AssignIfNull(ref Sunlight, this._Sunlight);
-
-            UnityNullExtensions.AssignIfNull(ref FpsTextLabel, this._FpsTextLabel);
         }
     }
 }
